f2892359f9add447ae8f83f67ef13b0f47748448
Аnna <admin@MacBook-Pro-Anna.local>
2022-12-22 16:59:00 +0300

    new var

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
 //меньше либо равна 3 символа
+/*
 string[] M;
             Console.WriteLine("Введите массив строк через пробел");
             string stroka = Console.ReadLine();
@@ -16,3 +17,28 @@ string[] M;
             }
             Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
             Console.ReadKey(true);
+            */
+            string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
